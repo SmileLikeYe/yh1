477228532343c71f4c357ecff141abb7b05cc331
yxd <qweqwe_40343@163.com>
2015-10-18 22:25:39 +0800

    minor

--- yhApp/www/js/controllers.js
+++ yhApp/www/js/controllers.js
@@ -2,42 +2,35 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   // Form data for the login modal
   $scope.loginData = {};
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
+	scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+	$scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+	$scope.modal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+	$scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+	console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+	// Simulate a login delay. Remove this and replace with your login
+	// code if using a login system
+	$timeout(function() {
+	  $scope.closeLogin();
+	}, 1000);
   };
 })
 
@@ -46,23 +39,26 @@ angular.module('starter.controllers', [])
 
 .controller('HomePageCtrl',function($scope){
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+	{ title: 'Reggae', id: 1 },
+	{ title: 'Chill', id: 2 },
+	{ title: 'Dubstep', id: 3 },
+	{ title: 'Indie', id: 4 },
+	{ title: 'Rap', id: 5 },
+	{ title: 'Cowbell', id: 6 }
   ];
   $scope.doRefresh = function() {
-    /*$http.get('/new-items')
-     .success(function(newItems) {
-       $scope.items = newItems;
-     })
-     .finally(function() {
-       // Stop the ion-refresher from spinning
-       $scope.$broadcast('scroll.refreshComplete');
-     });*/
-       $scope.$broadcast('scroll.refreshComplete');
+	/*$http.get('/new-items')
+	 .success(function(newItems) {
+	   $scope.items = newItems;
+	 })
+	 .finally(function() {
+	   // Stop the ion-refresher from spinning
+	   $scope.$broadcast('scroll.refreshComplete');
+	 });*/
+	   $scope.$broadcast('scroll.refreshComplete');
   };
+	$scope.click = function(){
+		
+	};
 	
 });
