904b4081d6d92a1e09c2dea2dae87e0694886b4e
Smile <SmileToYe@gmail.com>
2015-10-21 01:06:11 +0800

    添加拍照的函数 getPhoto

--- yhApp/www/js/controllers.js
+++ yhApp/www/js/controllers.js
@@ -122,9 +122,23 @@ $scope.doRefresh = function() {
 	 });*/
 	   $scope.$broadcast('scroll.refreshComplete');
 };
-	$scope.click = function(){
-		
-	};
-	
+$scope.click = function(){};
+$scope.getPhoto= function(){
+	console.log("sdfds");
+	navigator.camera.getPicture(onSuccess, onFail, { 
+		quality: 50,
+    	destinationType: Camera.DestinationType.FILE_URI 
+    });
+
+	function onSuccess(imageURI) {
+    	var image = document.getElementById('myImage');
+    	image.src = imageURI;
+	}
+
+	function onFail(message) {
+    	alert('Failed because: ' + message);
+	}
+
+};	
 });
 
