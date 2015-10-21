896147e1a3ca44d8dde2e35c5a5038da32b12a92
yxd <qweqwe_40343@163.com>
2015-10-22 04:04:27 +0800

    map page 2015/10/22

--- yhApp/www/js/app.js
+++ yhApp/www/js/app.js
@@ -78,6 +78,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
+    .state('app.map', {
+      url: '/map',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/map.html',
+          controller: 'mapCtrl'
+        }
+      }
+    })
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
--- yhApp/www/js/controllers.js
+++ yhApp/www/js/controllers.js
@@ -103,8 +103,12 @@ $scope.sendVCode = function() {
   
 })
 
-
-
+.controller('mapCtrl',function($scope){
+	var bmap=new BMap.Map("allmap");    // 创建Map实例map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+	bmap.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+	bmap.setCurrentCity("上海");          // 设置地图显示的城市 此项是必须设置的
+	$scope.map = bmap;
+})
 
 .controller('HomePageCtrl',function($scope){
 $scope.playlists = [
