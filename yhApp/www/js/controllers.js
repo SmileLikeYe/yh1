2cf9251cf13e2d614d98df5b29afd7e08979bd31
smile <SmileToYe@gmail.com>
2015-10-22 20:33:24 +0800

    add item back home

--- yhApp/www/js/controllers.js
+++ yhApp/www/js/controllers.js
@@ -152,6 +152,33 @@ $scope.getPhoto= function(){
     	alert('Failed because: ' + message);
 	}
 
+
+
+};
+
+$scope.getPosition= function(){
+  navigator.geolocation.getCurrentPosition(onSuccess,onFail, {
+    enableHighAccuracy: false,
+    timeout: 60*1000,
+    maximumAge: 1000*60*10
+  });
+
+  function onSuccess(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+  }
+
+  function onFail(message) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+  }
+
 };
 });
 
